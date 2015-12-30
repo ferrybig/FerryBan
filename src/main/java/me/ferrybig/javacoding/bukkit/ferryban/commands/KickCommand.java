7bfffc371b6559d33603129b04f748b2bb61d363
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-12-30 23:20:13 +0100

    Fix missing import

--- src/main/java/me/ferrybig/javacoding/bukkit/ferryban/commands/KickCommand.java
+++ src/main/java/me/ferrybig/javacoding/bukkit/ferryban/commands/KickCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
